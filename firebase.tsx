1201e8a5ffffceec8ab44791c8090de5873ea3d7
legitdaylight <jeremyd26937@gmail.com>
2025-07-28 15:06:28 -0400

    small change

--- firebase.tsx
+++ firebase.tsx
@@ -7,7 +7,7 @@ import { getAuth, GoogleAuthProvider} from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBBPGxGJiMnPWQ2gHiPSF_1Xdy3kiQoMiE",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "gengo-ae864.firebaseapp.com",
   projectId: "gengo-ae864",
   storageBucket: "gengo-ae864.firebasestorage.app",
