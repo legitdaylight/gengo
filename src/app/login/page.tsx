4b927cdf58bfbeafdfe43f05f7284688a73b2fb6
legitdaylight <jeremyd26937@gmail.com>
2025-07-28 15:01:46 -0400

    feat: add SignInButton component and integrate Google sign-in functionality in the login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,24 @@
 import { signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from "../../../firebase";
+import SignInButton from "../Components/SignInButton";
 
 export default function LoginPage() {
-    const handleGoogleLogin = async() => {
-        try {
-            const result = await signInWithPopup(auth, googleProvider);
-            console.log("User:", result.user); // Contains name, email, photo, etc.
-        } catch (err) {
-            console.error(err);
-        }
-    }
 
     return (
-        <div>
-            <h1 className="">Join GenGo and elevate your Japanese learning journey!</h1>
-            <div className="border border-solid border-gray-500 p-4 cursor-pointer">
-                <button onClick={handleGoogleLogin}>Sign in With Google</button>
+        <div className="flex h-screen">
+            <div className="w-1/3 bg-japan-500 flex flex-col justify-center items-center pb-48">
+                <div className="select-none text-justify pb-4 text-2xl text-white">
+                    <h1><span className="font-bold">Sign In</span> to Gengo</h1>
+                    <h2>and elevate your Japanese learning journey.</h2>
+                </div>
+                <div className="w-4/5 mb-2">
+                    <hr className="w-full border-t border-gray-300 my-2"/>
+                </div>
+                <SignInButton />
+            </div>
+
+            <div className="w-2/3 h-screen relative">
+                <img src="/tokyo.png" className="object-cover w-full h-full"></img>
             </div>
         </div>
     )
